e16923c517cb41605c984760ed6735088f893457
Eng-Ahmad-Almohammad <almohammedahmed2@gmail.com>
2022-01-30 14:32:25 +0200

    Add the demo

---
+++ Day11/demo/recipes/server.js
@@ -0,0 +1,46 @@
+`use strict`;
+
+const express = require('express');
+
+const app = express();
+
+const jsonData = require("./data.json");
+
+app.get('/', helloWorldHandler);
+
+app.get("/recipes", recipesHandler);
+
+function Recipe(title, readyInMinutes, summary, vegetarian, instructions, sourceUrl, image, id){
+    this.title = title;
+    this.readyInMinutes = readyInMinutes;
+    this.summary = summary;
+    this.vegetarian = vegetarian;
+    this.instructions = instructions;
+    this.sourceUrl = sourceUrl;
+    this.image = image;
+    this.id = id;
+}
+function helloWorldHandler(req, res){
+    return res.status(200).send("Hello World");
+};
+
+function recipesHandler(req, res){
+    let recipes = [];
+    for (i=0; i < data.data.length; i++){
+        console.log(jsonData.data[i]);
+    }
+
+    let x = jsonData.data.map( (value,index) => {
+        return value;
+    })
+    jsonData.data.map(value => {
+        let oneRecipe = new Recipe(value.title, value.readyInMinutes, value.summary, value.vegetarian, value.instructions, value.sourceUrl, value.image, value.id);
+        recipes.push(oneRecipe);
+    });
+
+    return res.status(200).json(recipes);
+};
+
+app.listen(3000, () => {
+    console.log('Listen to port 3000');
+})
